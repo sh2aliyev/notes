da7fa0b2b695863cf403ec44048f8b33c7b2aa2d
Sha'an <35802638+sh2aliyev@users.noreply.github.com>
2025-11-03 20:42:15 +0400

    setup: Change blockquote element styling to adapt my use case

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -4,6 +4,14 @@ import type { MDXComponents } from 'mdx/types';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    blockquote: ({ children, ...props }) => (
+      <blockquote
+        className="text-fd-muted-foreground text-sm font-normal not-italic *:before:content-none *:after:content-none"
+        {...props}
+      >
+        {children}
+      </blockquote>
+    ),
     ...components,
   };
 }
