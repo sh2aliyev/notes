10dfaddb24e9d8a98e9659e0249562d1ead8cbbc
Sha'an <35802638+sh2aliyev@users.noreply.github.com>
2025-11-04 16:23:48 +0400

    setup: Make custom icons possible

--- src/lib/source.ts
+++ src/lib/source.ts
@@ -1,12 +1,19 @@
 import { docs } from '@/.source';
 import { type InferPageType, loader } from 'fumadocs-core/source';
-import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
+import { icons } from 'lucide-react';
+import { createElement } from 'react';
+import { customIcons } from './consts';
 
 // See https://fumadocs.dev/docs/headless/source-api for more info
 export const source = loader({
   baseUrl: '/docs',
   source: docs.toFumadocsSource(),
-  plugins: [lucideIconsPlugin()],
+  plugins: [],
+  icon(icon) {
+    if (!icon) return;
+    if (icon in customIcons) return createElement(customIcons[icon as keyof typeof customIcons]);
+    if (icon in icons) return createElement(icons[icon as keyof typeof icons]);
+  },
 });
 
 export function getPageImage(page: InferPageType<typeof source>) {
