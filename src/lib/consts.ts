9a9efddc776f44dcab8d6cf58afa0faa59485434
Sha'an <35802638+sh2aliyev@users.noreply.github.com>
2025-11-05 00:25:55 +0400

    dsa: Add index page

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,4 +1,5 @@
 import { GitIcon } from '@/components/icons/git';
+import { Network } from 'lucide-react';
 
 export const customIcons = {
   GitIcon,
@@ -9,5 +10,10 @@ export const indexMetaMap = {
     title: 'Git',
     desc: 'Git is a free and open-source distributed version control system designed to manage changes to files and support coordinated work among developers.',
     icon: GitIcon,
+  },
+  dsa: {
+    title: 'Data Structures & Algorithms',
+    desc: 'Data Structures and Algorithms (DSA) is a fundamental concept in computer science that involves organizing and manipulating data efficiently to solve problems and perform tasks effectively.',
+    icon: Network,
   },
 };
