50f417ba47cd71ec83e8cbf894a0d3c0ce04fdf8
Sha'an Aliyev <35802638+sh2aliyev@users.noreply.github.com>
2026-01-31 02:57:26 +0400

    setup: Make icons colorful

--- src/components/icons/dsa.tsx
+++ src/components/icons/dsa.tsx
@@ -2,15 +2,39 @@ import { SVGProps } from 'react';
 
 export function DsaIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>
-      {/* Icon from Solar by 480 Design - https://creativecommons.org/licenses/by/4.0/ */}
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 14 14" {...props}>
+      {/* Icon from Streamline color by Streamline - https://creativecommons.org/licenses/by/4.0/ */}
       <title>DSA</title>
-      <path
-        fill="currentColor"
-        fillRule="evenodd"
-        d="M7.907 4.256a3.001 3.001 0 1 0-3.651 3.651A1 1 0 0 0 4.25 8v8q0 .048.006.093a3.001 3.001 0 1 0 3.651 3.651q.045.006.093.006h8a1 1 0 0 0 .093-.006a3.001 3.001 0 1 0 3.651-3.651A1 1 0 0 0 19.75 16V8a1 1 0 0 0-.006-.093a3.001 3.001 0 1 0-3.651-3.651A1 1 0 0 0 16 4.25H8a1 1 0 0 0-.093.006M5.744 7.907q.006.045.006.093v8a1 1 0 0 1-.006.093a3 3 0 0 1 2.163 2.163A1 1 0 0 1 8 18.25h8a1 1 0 0 1 .093.006a3 3 0 0 1 2.163-2.163A1 1 0 0 1 18.25 16V8q0-.048.006-.093a3 3 0 0 1-2.163-2.163A1 1 0 0 1 16 5.75H8a1 1 0 0 1-.093-.006a3 3 0 0 1-2.163 2.163"
-        clipRule="evenodd"
-      />
+      <g fill="none">
+        <path
+          fill="#d7e0ff"
+          fillRule="evenodd"
+          d="M.65 8.074v3.62l3.167 1.357l3.167-1.357l3.166 1.357l3.167-1.357v-3.62l-.008-.003l-3.159 1.353l-3.166-1.357l3.166-1.35l.002-3.621l-.002.026L6.983 4.48L3.82 3.123l-.002 3.594l3.167 1.35l-3.167 1.359z"
+          clipRule="evenodd"
+        />
+        <path
+          fill="#fff"
+          d="m6.985 1.738l3.167 1.357l-.002.027l-3.167 1.357l-3.166-1.357l.002-.027zm3.165 4.979l3.158 1.354l-3.158 1.353l-3.166-1.357l-3.167 1.359L.65 8.074l3.167-1.357l3.167 1.35z"
+        />
+        <path
+          stroke="#4147d5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m.65 8.067l3.167 1.357l3.167-1.357M3.817 3.122l3.166 1.357l3.167-1.357"
+        />
+        <path
+          stroke="#4147d5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m6.983 8.067l3.167 1.357l3.167-1.357M.65 11.694v-3.62l3.167-1.357l3.167 1.357v3.62L3.817 13.05z"
+        />
+        <path
+          stroke="#4147d5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.819 6.715v-3.62l3.166-1.357l3.167 1.357v3.62L6.985 8.072zm3.164 4.979v-3.62l3.167-1.357l3.167 1.357v3.62L10.15 13.05zM3.817 9.426v3.625m6.335-3.625v3.625M6.983 4.48v3.624"
+        />
+      </g>
     </svg>
   );
 }
